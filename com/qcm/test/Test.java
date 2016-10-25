2b35fddda1b12fa1d83d8418da6a1846c1caa1b0
裘丛民 <qcm3771787@icloud.com>
2016-10-25 11:23:42 +0800

    表改过了，所以配置文件也要该

--- com/qcm/test/Test.java
+++ com/qcm/test/Test.java
@@ -5,14 +5,13 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import com.qcm.constant.Constant;
 import com.qcm.entity.News;
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("123s");
 		ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
-				Constant.XML_LOCATION);
+				"src/main/java/springmvc-servlet.xml");
 		SessionFactory sessionFactory = applicationContext.getBean(
 				"sessionFactory", SessionFactory.class);
 		Session session = sessionFactory.openSession();
