d4260bcfb3f80944773fdc692534d83aa13287c3
郑利斌 <545696314@qq.com>
2016-10-25 17:37:09 +0800

    abc

--- com/qcm/constant/Constant.java
+++ com/qcm/constant/Constant.java
@@ -2,6 +2,6 @@ package com.qcm.constant;
 
 public class Constant {
 
-	// springmvc.xml的路劲
+	// springmvc.xml的路径
 	public static final String XML_LOCATION = "src/springmvc-servlet.xml";
 }
--- com/qcm/controller/LoginAndRegister.java
+++ com/qcm/controller/LoginAndRegister.java
@@ -10,18 +10,15 @@ import com.qcm.entity.Admin;
 import com.qcm.entity.Counter;
 
 @Controller
-public class Login {
+public class LoginAndRegister {
 	@RequestMapping(value="/login")
-	public String adminLogin(HttpServletRequest req,@ModelAttribute Admin admin,@ModelAttribute Counter counter){
-		admin.setAdminName(req.getParameter("username"));
-		counter.setUserName(req.getParameter("username"));
-		admin.setAdminPassword(req.getParameter("password"));
-		counter.setUserPassword(req.getParameter("password"));
+	public String login(HttpServletRequest req,@ModelAttribute Admin admin){
+		//获取单选的值
 		String power = req.getParameter("power");
 		System.out.println(power);
 		if("counter".equals(power)){
-			System.out.println(counter.getUserName()+"==="+counter.getUserPassword());
-			if("123".equals(counter.getUserName())&&"123".equals(counter.getUserPassword())){
+			System.out.println(admin.getAdminName()+"==="+admin.getAdminPassword());
+			if("123".equals(admin.getAdminName())&&"123".equals(admin.getAdminPassword())){
 				return "lalala";
 			}else{
 				req.getSession().setAttribute("info", "用户名或密码错误!");
@@ -37,5 +34,10 @@ public class Login {
 				return "index";
 			}
 		}
+	}
+	@RequestMapping(value="/login")
+	public String register(@ModelAttribute Counter counter){
+		
+		return "";
 	}
 }
